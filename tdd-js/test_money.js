33cd8e69ca4c92e30472190cd05fbf4108a4287c
Martossy Alex <github@mail.martossy.hu>
2022-11-29 15:17:35 +0100

    comments

--- tdd-js/test_money.js
+++ tdd-js/test_money.js
@@ -29,14 +29,17 @@ class Huf extends Currency {
 const assert = require("assert");
 const internal = require("stream");
 
+// 5 USD * 2 = 10 USD
 let fiveUSD = new Dollar(5);
 let tenUSD = fiveUSD.times(2);
 assert.strictEqual(tenUSD.amount, 10);
 
+// 10 EUR * 2 = 20 EUR
 let tenEUR = new Euro(10);
 let twentyEUR = tenEUR.times(2);
 assert.strictEqual(twentyEUR.amount, 20);
 
+//10 EUR * 2 = 20 EUR
 tenEUR = new Currency(10, 'EUR');
 twentyEUR = tenEUR.times(2);
 assert.strictEqual(twentyEUR.amount, 20);
