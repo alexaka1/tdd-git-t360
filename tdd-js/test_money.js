84d50ea72c124a2c0868db29816a0e5727ccc84e
Martossy Alex <github@mail.martossy.hu>
2022-11-29 14:59:46 +0100

    classes, basic reqs

--- tdd-js/test_money.js
+++ tdd-js/test_money.js
@@ -1,18 +1,23 @@
-console.log(
-  ([][
-    (!![] + [])[!+[] + !+[] + !+[]] +
-      ([][[]] + [])[+!+[]] +
-      (!![] + [])[+[]] +
-      (!![] + [])[+!+[]] +
-      ([![]] + [][[]])[+!+[] + [+[]]] +
-      (!![] + [])[!+[] + !+[] + !+[]] +
-      (![] + [])[!+[] + !+[] + !+[]]
-  ]() + [])[!+[] + !+[]] +
-    (![] + [])[+!+[]] +
-    ([][[]] + [])[+!+[]] +
-    (![] + [])[+!+[]] +
-    ([][[]] + [])[+!+[]] +
-    (![] + [])[+!+[]]
-);
+class Currency{
+    constructor(amount) {
+        this.amount = amount;
+    }
+}
+class Dollar extends Currency {
+}
+
+class Euro extends Currency {
+}
+
+
 
 const assert = require("assert");
+
+let fiveUSD = new Dollar(5);
+let tenUSD = fiveUSD * 2;
+assert.strictEqual(tenUSD.amount, 10);
+
+let tenEUR = new Euro(5);
+let twentyEUR = tenEUR * 2;
+assert.strictEqual(twentyEUR.amount, 20);
+
