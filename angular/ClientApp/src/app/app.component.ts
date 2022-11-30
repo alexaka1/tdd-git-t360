e41a8fe5cc7bbef96d949e73c721a861b96a7f15
Martossy Alex <github@mail.martossy.hu>
2022-11-30 11:22:37 +0100

    portfolio féleség

--- angular/ClientApp/src/app/app.component.ts
+++ angular/ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Currency, Money, MoneyService} from "../services/money.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ClientApp';
+  moneys: Observable<Currency[]>;
+  amount: number | null = null;
+  currency: string | null = null;
+  currencies: string[] = [
+    'USD','EUR','GBP','JPY','AUD','CAD','CHF','CNY','HKD','NZD','SEK','SGD','ZAR','HUF'
+  ].sort((a, b) => a.localeCompare(b));
+
+  constructor(private moneyService: MoneyService) {
+    this.moneys = moneyService.list();
+  }
+
+  add() {
+    if (this.amount && this.currency) {
+      this.moneyService.add({
+        amount: this.amount,
+        currency: this.currency,
+      });
+      this.reset();
+  }
+
+  }
+  remove(index: number){
+    this.moneyService.remove(index);
+  }
+  reset(){
+    this.amount = null;
+    this.currency = null;
+  }
 }
+
---
+++ angular/ClientApp/src/services/money.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoneyService {
+
+  private moneys: Currency[] = [];
+  constructor() { }
+
+  add(money: Money){
+    const max = Math.max(...this.moneys.map(m => m.id));
+    let id = 0;
+    if (max !== NaN && max !== -Infinity && max !== Infinity) {
+      id = max + 1;
+    }
+    this.moneys.push({
+      id: id,
+      ...money
+    });
+  }
+  remove(id: number){
+    this.moneys.splice(this.moneys.findIndex(m=> m.id === id), 1);
+  }
+
+  list(): Observable<Currency[]>{
+    return of(this.moneys);
+  }
+}
+
+export interface Money {
+  amount: number;
+  currency: string;
+  // constructor(id: number, amount: number, currency: string) {
+  //   this.id = id;
+  //   this.amount = amount;
+  //   this.currency = currency;
+  // }
+}
+export class Currency implements Money {
+  id: number;
+  amount: number;
+  currency: string;
+
+  constructor(id: number, amount: number, currency: string) {
+    this.id = id;
+    this.amount = amount;
+    this.currency = currency;
+  }
+}
+
+export class Portfolio {
+  private moneys: Money[];
+  constructor(moneys: Money[]){
+    this.moneys = moneys;
+  }
+  // evaluate(currency) {
+  //   let result = this.moneys.reduce(function (prev, curr) {
+  //     if (prev.currency !== "USD") {
+  //       prev = Portfolio.convert(prev, "USD");
+  //     }
+  //     if (curr.currency !== "USD") {
+  //       curr = Portfolio.convert(curr, "USD");
+  //     }
+  //     switch (curr.meth) {
+  //       case "add":
+  //         return new Currency(
+  //           prev.amount + curr.amount,
+  //           "USD"
+  //         );
+  //       default:
+  //         return new Currency(
+  //           prev.amount + curr.amount,
+  //           "USD"
+  //         );
+  //     }
+  //   });
+  //   return Portfolio.convert(result, currency);
+  // }
+  // static convert(money, currency) {
+  //   switch (money.currency) {
+  //     case "EUR":
+  //       money = new Dollar(money.amount * 1.2);
+  //       break;
+  //     case "HUF":
+  //       money = new Dollar(money.amount / 1100);
+  //       break;
+  //   }
+  //   switch (currency) {
+  //     case "EUR":
+  //       money = new Euro(money.amount / 1.2);
+  //       break;
+  //     case "HUF":
+  //       money = new Huf(money.amount * 1100);
+  //       break;
+  //   }
+  //   return money;
+  // }
+}
